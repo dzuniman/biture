16969687688f3d4bcae507de1b564057a5f677c4
dzunisani <dzuniman@gmail.com>
2026-06-03 20:49:11 +0200

    Fix types and build config again

--- Quote2Cash.Domain/obj/Debug/net8.0/Quote2Cash.Domain.AssemblyInfo.cs
+++ Quote2Cash.Domain/obj/Debug/net8.0/Quote2Cash.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quote2Cash.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e81ff869ea305a3bbd38765a81e7d141c6543b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1d78f69e179836883c92d52b4e3352e39d4d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quote2Cash.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quote2Cash.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
