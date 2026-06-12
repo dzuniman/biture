c32f47b66d909214ee909b464efe0bb8772fb09b
dzunisani <dzuniman@gmail.com>
2026-06-12 17:26:01 +0200

    Migrations

---
+++ Quote2Cash.Persistence/Quote2CashDbContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Quote2Cash.Persistence.Data;
+
+public class Quote2CashDbContextFactory : IDesignTimeDbContextFactory<Quote2CashDbContext>
+{
+    public Quote2CashDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<Quote2CashDbContext>();
+
+        // Use your local dev connection string here
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Quote2Cash;Username=postgres;Password=password");
+
+        return new Quote2CashDbContext(optionsBuilder.Options);
+    }
+}
--- Quote2Cash.Persistence/obj/Debug/net8.0/Quote2Cash.Persistence.AssemblyInfo.cs
+++ Quote2Cash.Persistence/obj/Debug/net8.0/Quote2Cash.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af0007c5ee4c5cf59753d96e24fa2c80923724c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901d85d6866bb09d7cd054817c385dbbd18f2adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
