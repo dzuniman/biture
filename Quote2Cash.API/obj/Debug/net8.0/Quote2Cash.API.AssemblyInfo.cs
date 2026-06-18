3496716de841e1c165e39b27e762fc35f80b97a8
dzunisani <dzuniman@gmail.com>
2026-06-18 08:32:56 +0200

    Merge main into dev

--- Quote2Cash.API/obj/Debug/net8.0/Quote2Cash.API.AssemblyInfo.cs
+++ Quote2Cash.API/obj/Debug/net8.0/Quote2Cash.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quote2Cash.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63ccf16c142670d5b76538a3ca938893bcfbe9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5b8672979904af93117d8960a7ff6246f761c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quote2Cash.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quote2Cash.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
