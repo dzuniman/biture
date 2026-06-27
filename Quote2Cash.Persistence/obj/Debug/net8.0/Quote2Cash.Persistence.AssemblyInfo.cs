938b8c60a7c301098bdf3b3d527734df7e0ecaae
dzunisani <dzuniman@gmail.com>
2026-06-27 16:46:53 +0200

    Stash

--- Quote2Cash.Persistence/obj/Debug/net8.0/Quote2Cash.Persistence.AssemblyInfo.cs
+++ Quote2Cash.Persistence/obj/Debug/net8.0/Quote2Cash.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2abd0d3067c52f7bdb913b4e6b3d08b7dbdfa6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb2f0c876bfba99affba47c1b8d02eafe544f58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quote2Cash.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
