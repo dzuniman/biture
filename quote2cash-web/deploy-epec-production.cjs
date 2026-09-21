d581e9045b7db5282e3a8883300c732ddffa13ef
Dzunisani <dzuniman@gmail.com>
2026-09-21 12:56:23 +0200

    Fixed deployment environment files

--- quote2cash-web/deploy-epec-production.cjs
+++ quote2cash-web/deploy-epec-production.cjs
@@ -2,9 +2,9 @@ const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-    user: "biturvnu",
-    password: "2X66ei72gGwzE",
-    host: "ftp.erp.biture.co.za",
+    user: "epeccynx",
+    password: "EPe@98750!",
+    host: "ftp.erp.epec.co.za",
     port: 21,
     localRoot: __dirname + "/dist",
     remoteRoot: "/public_html/erp_epec",
